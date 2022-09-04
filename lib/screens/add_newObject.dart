a0098e8490c05dde8333da448e6a2c35fe3882a6
Yann Jollien <jollienyann@yahoo.fr>
2022-09-04 21:31:21 +0200

    [AddItem] Add new item to db

--- lib/screens/add_newObject.dart
+++ lib/screens/add_newObject.dart
@@ -1,5 +1,7 @@
 
 import 'package:flutter/material.dart';
+import 'package:sam/db/database.dart';
+import 'package:sam/home_page.dart';
 
 class AddObject extends StatefulWidget {
 
@@ -10,6 +12,7 @@ class AddObject extends StatefulWidget {
 class _AddObjectState extends State<AddObject> {
 
   String newValue = "";
+  TextEditingController itemController = new TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -17,41 +20,51 @@ class _AddObjectState extends State<AddObject> {
       appBar: AppBar(
         title: Text('Add object')
       ),
-      body: Column(
-        children: [
-          TextField(
-              decoration: InputDecoration(
-                labelText: "Item name", //babel text
-                hintText: "Enter item name", //hint text
-                hintStyle: TextStyle(fontSize: 18, fontWeight: FontWeight.bold), //hint text style
-                labelStyle: TextStyle(fontSize: 18, color: Colors.cyan), //label style
+      body: Padding(
+        padding: EdgeInsets.all(36.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: itemController,
+                decoration: InputDecoration(
+                  labelText: "Item name", //babel text
+                  hintText: "Enter item name", //hint text
+                  hintStyle: TextStyle(fontSize: 18, fontWeight: FontWeight.bold), //hint text style
+                  labelStyle: TextStyle(fontSize: 18, color: Colors.cyan), //label style
+                ),
+            ),
+            ListTile(
+              title: const Text('Category 1'),
+              leading: Radio(
+                value: "1",
+                groupValue: newValue,
+                onChanged: (value) {
+                  setState(() {
+                    newValue = value.toString();
+                  });
+                },
               ),
-          ),
-          ListTile(
-            title: const Text('Category 1'),
-            leading: Radio(
-              value: "1",
-              groupValue: newValue,
-              onChanged: (value) {
-                setState(() {
-                  newValue = value.toString();
-                });
-              },
             ),
-          ),
-          ListTile(
-            title: const Text('Category 2'),
-            leading: Radio(
-              value: "2",
-              groupValue: newValue,
-              onChanged: (value) {
-                setState(() {
-                  newValue = value.toString();
-                });
-              },
+            ListTile(
+              title: const Text('Category 2'),
+              leading: Radio(
+                value: "2",
+                groupValue: newValue,
+                onChanged: (value) {
+                  setState(() {
+                    newValue = value.toString();
+                  });
+                },
+              ),
             ),
-          ),
-        ],
+            ElevatedButton(
+                onPressed: () {
+                  print("Value from category"+newValue);
+                  DBHelper.saveListObject(new ListObject("44", itemController.text, "v100", newValue, "0xee29"));
+                },
+                child: Text("SaveValues"))
+          ],
+        ),
       ),
     );
   }
